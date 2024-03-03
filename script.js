be0460a7d5fc8ebc977d965caa71d66b2d3f2ab8
Ostrinka <148815239+Ostrinka@users.noreply.github.com>
2024-03-04 00:18:50 +0200

    done

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ const list = document.querySelector(".todo-list");
 button.addEventListener("click", () => {
   const li = document.createElement("li");
   li.className = "todo-list-item";
+  li.textContent = input.value;
   const deleteBtn = document.createElement("button");
   deleteBtn.textContent = "Delete";
-  li.textContent = input.value;
-  list.appendChild(li);
   li.appendChild(deleteBtn);
+  list.appendChild(li);
   input.value = "";
 
   deleteBtn.addEventListener("click", () => {
